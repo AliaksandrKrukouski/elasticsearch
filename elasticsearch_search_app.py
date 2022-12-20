f0b8703936980eed22d6042d91ab6122585057ec
Aliaksandr Krukouski <v-akrukouski@expediagroup.com>
2022-12-19 23:15:55 -0600

    Search app finishing dev

--- elasticsearch_search_app.py
+++ elasticsearch_search_app.py
@@ -1,4 +1,5 @@
 import csv
+import json
 import os
 import sys
 import uuid
@@ -27,12 +28,13 @@ INDEX_MAPPING = {
 }
 
 def create_index(es, idx_name, idx_mapping):
-    print(f"Name: {idx_name}\nSchema: {idx_mapping}")
-    if not es.indices.exists(index=idx_name):
-        es.indices.create(index=idx_name, mappings=idx_mapping)
-        print(f"{idx_name} index created")
-    else:
-        print(f"{idx_name} index already exists")
+    print(f"Name: {idx_name}\nSchema: {json.dumps(idx_mapping, indent=2)}")
+    if es.indices.exists(index=idx_name):
+        print(f"The index already exists. Drop it...")
+        es.indices.delete(index=idx_name)
+
+    es.indices.create(index=idx_name, mappings=idx_mapping)
+    print(f"The index created")
 
 def gen_docs(idx_name, csv_file_path):
     print(f"Index: {idx_name}\nCSV file: {csv_file_path}")
@@ -49,27 +51,103 @@ def gen_docs(idx_name, csv_file_path):
             yield doc
 
 def run_search(es, idx_name):
-    prompt = "Choose search type:"
-    prompt += "\n1 - Match query"
-    prompt += "\n2 - Term query"
-    prompt += "\nEnter 0 to end the program: "
 
-    search_input = int(input(prompt))
+    while True:
+        prompt = "Choose search condition (0 to exit):"
+        prompt += "\n1 - Search by title (match query)"
+        prompt += "\n2 - Search by title (reqexp query)"
+        prompt += "\n3 - Search by title (wildcard query)"
+        prompt += "\n4 - Search by country (term query)"
+        prompt += "\n5 - Search by release date interval (range query)"
+        prompt += "\n6 - Search by several columns (multi-match query)"
+        prompt += "\n7 - Search by title, country and release date (boolean query)"
+        prompt += "\n>>> "
 
-    if search_input == 1:
-        print("*** MATCH QUERY ***")
-        title_input = input("Input title for search: ")
-        query = {
-            "query": {
+        in_condition = int(input(prompt))
+
+        if in_condition == 0:
+            print("\nFinishing the program.")
+            break
+        elif in_condition == 1:
+            in_query_val = input("\nSearch title: ")
+            query = {
                 "match": {
-                    "title": {"query": title_input, "operator": "or"},
+                    "title": {"query": in_query_val, "operator": "or"}
                 }
             }
-        }
+        elif in_condition == 2:
+            in_reqexp_val = input("\nRegexp title: ")
+            query = {
+                "regexp": {
+                    "title": in_reqexp_val
+                }
+            }
+        elif in_condition == 3:
+            in_wildcard_val = input("\nWildcard title: ")
+            query = {
+                "wildcard": {
+                    "title": in_wildcard_val + '*'
+                }
+            }
+        elif in_condition == 4:
+            in_query_val = input("\nSearch country: ")
+            query = {
+                "term": {
+                    "country": {"value": in_query_val}
+                }
+            }
+        elif in_condition == 5:
+            in_gte_val = input("\nSearch year FROM: ")
+            in_lte_val = input("Search year TO: ")
+            query = {
+                "range": {
+                    "release_year": {"gte": in_gte_val,
+                                     "lte": in_lte_val}
+                }
+            }
+        elif in_condition == 6:
+            in_query_val = input("\nSearch text: ")
+            in_fields_val = input("Search fields (separated by comma): ")
 
-    print(f"Search query: {query}")
-    response = es.search(index=idx_name, body=query)
-    print(f"Search result: {response}")
+            fields = [f.strip() for f in in_fields_val.split(",")]
+
+            query = {
+                "multi_match": {
+                    "query": in_query_val,
+                    "fields": fields
+                }
+            }
+        elif in_condition == 7:
+            in_title_val = input("\nSearch title: ")
+            in_country_val = input("Search country: ")
+            in_date_val = input("Search year: ")
+
+            query = {
+                "bool": {
+                    "must": [
+                        {
+                            "match": {
+                                "title": {"query": in_title_val, "operator": "or"}
+                            }
+                        },
+                        {
+                            "term": {
+                                "country": {"value": in_country_val}
+                            }
+                        },
+                        {
+                            "term": {
+                                "release_year": {"value": in_date_val}
+                            }
+                        }
+                    ]
+                }
+            }
+
+        print(f"Query: {query}")
+
+        resp = es.search(index=idx_name, query=query)
+        print(f"Result: {json.dumps(resp['hits'], indent=2)}")
 
 def post_docs(es, docs):
     es_resp = es_helper.bulk(client=es, actions=docs)
